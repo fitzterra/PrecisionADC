fdda4648c8c639e6cb8bb061dfcf7221cf1da094
fitzterra <github_subs@icave.net>
2016-12-02 04:59:18 +0200

    Make the serial decoder more generic.
    Since the Arduino IDE has a very basic serial console, the previous
    state machine used for a serial decoder, that also supported a more
    intuative calibration function via a real serial terminal, was not
    ideal.
    
    The new serial decoder supports a key scan table that can be support
    both a limited, and proper serial terminal. It's probably overkill for
    using with a limited terminal, but much more intuative when using a real
    serial terminal, and can now accomodate both terminal types by
    specifying alternative keys for commands.
    
    Also add carraige returns to all line feeds used in calibration method
    to ensure correct display on serial terminals that does not
    automatically do a CR when receiving a LF (like screen).
    
    Also clean up some trailing whitespace on a few lines.

--- PrecisionADC.cpp
+++ PrecisionADC.cpp
@@ -16,35 +16,35 @@ bgMem bgEEPROMID {"bgID"}; // Struct that will be used as BG ref EEPROM ID
  * calculate the real analog input:
  *
  * Vinput = (readVcc * pinVal) / 1023  -> in mV
- * 
+ *
  * Credit here: https://code.google.com/p/tinkerit/wiki/SecretVoltmeter
  * with changes to make it work on other ATmega MCUs by various people in the
  * comments.
  *
  * Copying from the link above:
- * 
+ *
  * The ATmega MCUs has an internal switch that selects which pin the analogue
  * to digital converter reads. That switch has a few leftover connections, so
  * the chip designers wired them up to useful signals. One of those signals is
  * the 1.1V precision reference that may be used as Vref for ADC measurements.
- * 
+ *
  * So if you measure how large the known 1.1V reference is in comparison to Vcc,
  * you can back-calculate what Vcc is with a little algebra. That is how this
  * works.
- * 
+ *
  * From the equation in section 24.7 of the ATmega data sheet giving the
  * formula used to get the ADC input value:
- * 
+ *
  * ADC = (Vin * 1024) / Vref
- * 
+ *
  * This function internally connects Vref to Vcc and the input to the ADC comes
  * from the internal 1.1V reference. Thus, rearranging this equation and
  * expressing voltage in millivolts gives:
- * 
+ *
  * Vcc = (1100 * 1024) / ADC
- * 
+ *
  * or
- * 
+ *
  * Vcc = 1126400L / ADC
  *
  * Although this helps give a very accurate VCC value, the bandgap reference
@@ -68,13 +68,13 @@ uint32_t PrecisionADC::bgADC() {
 	ADMUX = _BV(MUX3) | _BV(MUX2);
 	#else
 	ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
-	#endif 
+	#endif
 
 	delay(2); // Wait for Vref to settle
 	ADCSRA |= _BV(ADSC); // Start conversion
 	while (bit_is_set(ADCSRA,ADSC)); // measuring
 
-	uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH 
+	uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH
 	uint8_t high = ADCH; // unlocks both
 
 	uint32_t result = (high<<8) | low;
@@ -85,97 +85,102 @@ uint32_t PrecisionADC::bgADC() {
 /**
  * Private method to read and decode keyboard input from the serial port.
  *
- * Since some control keys like the arrow keys are multi character inputs, this
- * method will try to decode these inputs as the characters are received. It
- * will wait up to tout millis between receiving input, and will return if
- * nothing was been received or no full input could be decoded.
+ * When using a real terminal (unlike the serial console used by the Arduino
+ * IDE), it is much easier to allow single key presses to issue commands
+ * (instead of having the press enter after every command character), and more
+ * intuative to use the up and down arrows to make adjustments, and the Escape
+ * key to exit an operation.
  *
- * This method will scan for the following keys and return the values indicated
- * (see the header for for the key defines):
- * up arrow   -> KEY_UP
- * down arrow -> KEY_DOWN
- * space bar  -> KEY_SPACE
- * escape     -> KEY_ESC
- * 1          -> KEY_ONE
- * 2          -> KEY_TWO
+ * Such a terminal will send multi character codes to represent certain control
+ * keys like arrows, page up/down, etc. To make it worse, these multi character
+ * codes can start with the decimal value 27 which is also the code for the
+ * escape key. This makes decoding all these keys a bit more complex.
+ *
+ * This method tries to make that easier by allowing a key table (keyTab) to
+ * define all keys, and possible multi character codes for special keys, to
+ * monitor the serial input for and decode to single integer value which is the
+ * index in the keyTab of the input identified.
+ *
+ * See the header for a description of defining keyTab and setting macros to
+ * make it easier to identify the decoded input.
  *
  * @param tout: Amount of time (millis) to wait for input on the serial line.
  *              This time should be just long enough to get all bytes for a
  *              multi-byte key press. The default is 100ms which should be fine.
+ * 
+ * @return : NO_KEY (-1) if no key press was available, or else the index into
+ *           keyTab for the decoded keypress.
  **/
-uint8_t PrecisionADC::readSerial(uint8_t tout) {
+int8_t PrecisionADC::readSerial(uint8_t tout) {
+    // We bail if there is no serial input
+    if(!Serial.available())
+        return NO_KEY;
+
+    // Buffer and index
+    uint8_t buf[KBUFSZ] = {};
+    uint8_t bufIdx = 0;
     // Preset the timeout millis
     uint32_t exitAt = millis() + tout;
-    uint8_t in;
-    // State machine current state representation. The states are:
-    // 'A' : start
-    // 'B' : escape code received (final)
-    // 'C' : extended code received
-    // 'D' : up arrow pressed (final)
-    // 'E' : down arrow pressed (final)
-    // 'F' : space key (final)
-    // 'G' : numeric 1 key (final)
-    // 'H' : numeric 2 key (final)
-    char state = 'A';
+    // Will be set to the index in keyTab for the key matched from serial input
+    // or -1 (NO_KEY) if no matches.
+    int8_t matchedKey = NO_KEY;
+    uint8_t partial;  // WIll count partial matches from the keyTab
+    uint8_t c = 0;
 
-    // Keep looping if not timed out yet
     while(millis() < exitAt) {
         // Any serial data?
         if(!Serial.available()) continue;
-        // There is something available, so reset the timer and get the value
-        exitAt = millis() + tout;
-        in = Serial.read();
-        switch (in) {
-            case 27:    // Escape code - either from the key or extended start
-                // We advance only if current state is 'A', else we reset
-                state = state=='A' ? 'B' : 'A';
-                break;
-            case 91:    // Extended code indicator if we're in state B
-                // We advance only if current state is 'B', else we reset
-                state = state=='B' ? 'C' : 'A';
-                break;
-            case 65:    // Up arrow if we're in state C
-            case 66:    // Down arrow if we're in state C
-                if (state=='C') {
-                    // We're in state 'D' or 'E' which are final states, so
-                    // we can return here
-                    return in==65 ? KEY_UP : KEY_DOWN;
-                }
-                // Reset state
-                state = 'A';
-                break;
-            case 32:    // Space if we're in state 'A'
-                if (state=='A') {
-                    // We're in state 'F' which is a final state, so
-                    // we can return here
-                    return KEY_SPACE;
-                }
-                // Reset state
-                state = 'A';
-                break;
-            case 49:    // Number '1' key if we're in state 'A'
-            case 50:    // Number '2' key if we're in state 'A'
-                if (state=='A') {
-                    // We're in state 'G' or 'H' which are final states, so
-                    // we can return here
-                    return in==49 ? KEY_ONE : KEY_TWO;
-                }
-                // Reset state
-                state = 'A';
-                break;
-            default:
-                // Reset for anything else
-                state = 'A';
+        // Read it into the next spot in the buffer.
+        buf[bufIdx] = Serial.read();
+        // Ignore any CR or LF chars in case the serial terminal sends them
+        if (buf[bufIdx]==CR or buf[bufIdx]==LF) {
+            buf[bufIdx]=0;
+            continue;
         }
-    }
+        // Reset the timer
+        exitAt = millis() + tout;
 
-    // We could possibly be in state B here (escape pressed)
-    if (state == 'B') {
-        return KEY_ESC;
+        // Try to match the buffer in the key table
+        matchedKey = NO_KEY;
+        partial = 0;
+        for (uint8_t k=0; k<keyTabLen; k++) {
+            // If there is no match on the first char, don't even bother
+            if(buf[0] != keyTab[k][0]) {
+                continue;
+            }
+            // First chars match, test how much of the buffer matches this key
+            // definition , keeping in mind that both the buffer and key defs
+            // will have all unused/trailing array elements set to 0
+            c = 0;
+            while(c<KBUFSZ && keyTab[k][c]==buf[c]) {
+                c++;
+            }
+            // A complete match will leave our char index at KBUFSZ
+            if (c==KBUFSZ) {
+                matchedKey = k;
+            } else {
+                // It's a partial match
+                partial++;
+            }
+        }
+        // If we have an exact match, and the serial buffer is empty, we
+        // can return the match
+        if(matchedKey!=NO_KEY && partial==0 && !Serial.available()) {
+            return matchedKey;
+        }
+        // If partial is 0 at this stage, then we know we have also not matched
+        // anything, but we have already read at least one char from serial, so
+        // there is no way to make any more matches, so we can return NO_KEY.
+        // Also, if we have read KBUFSZ chars already, then there is no match,
+        // so we can return NO_KEY
+        if (partial==0 || bufIdx==KBUFSZ) {
+            return NO_KEY;
+        }
+        // Otherwise we try to read another key into the buffer
+        bufIdx++;
     }
-    
-    // Indicate no valid input was received
-    return 0;
+    // After a timeout we can simply return matchedKey
+    return matchedKey;
 }
 
 /**
@@ -259,19 +264,19 @@ uint16_t PrecisionADC::readVcc() {
 void PrecisionADC::calibrateBG() {
     const uint16_t updateFreq = 1000;   // Live Vcc update frequency - mSecs
     char state = FT_STATE_MENU;         // Start off showing the menu
-    uint8_t key;                        // Will represent key pressed
+    int8_t key;                        // Will represent key pressed
     uint32_t nextUpdate = millis() + updateFreq; // Next millis to update VCC
-    const char menu[] = "\n== Bandgap fine tuning ==\n" \
-                        "[Space] to enter fine tuning display.\n" \
-                        "[1] to retrieved saved bandgap value from EEPROM.\n" \
-                        "[2] to save current bandgap value to EEPROM.\n" \
-                        "[Escape] to exit fine tuning.\n\n" \
-                        "While in fine tuning display, press:\n"\
-                        "[Space] to return to this menu.\n" \
-                        "[Up/Down arrows] to adjust bandgap voltage while\n" \
-                        "  measuring the supply voltage (VCC) externally with\n" \
-                        "  a multimeter.\n\n" \
-                        "[Space], [1], [2] or [Escape]?\n\n";
+    const char menu[] = "\n\r== Bandgap calibration ==\n\r" \
+                        "[Space] to enter calibration display.\n\r" \
+                        "[1] to retrieved saved bandgap value from EEPROM.\n\r" \
+                        "[2] to save current bandgap value to EEPROM.\n\r" \
+                        "[Escape]/[q] to exit calibration.\n\r\n\r" \
+                        "While in calibration display, press:\n\r"\
+                        "[Space] to return to this menu.\n\r" \
+                        "[Up/Down arrows]/[k or j] to adjust bandgap voltage while\n\r" \
+                        "  measuring the supply voltage (Vcc) externally with\n\r" \
+                        "  a multimeter.\n\r\n\r" \
+                        "[Space], [1], [2] or [Escape]/[q]?\n\r\n\r";
 
     // Show the menu on entry
     Serial.print(menu);
@@ -280,7 +285,7 @@ void PrecisionADC::calibrateBG() {
         // Check for serial key input using default timeout
         key = readSerial();
         // Did we get anything
-        if (key == 0) {
+        if (key == NO_KEY) {
             // No input. In menu state we do nothing, in tuning state we update
             // live Vcc display if needed
             if (state==FT_STATE_TUNE && millis()>=nextUpdate) {
@@ -289,17 +294,17 @@ void PrecisionADC::calibrateBG() {
                 Serial.print(readVcc());
                 Serial.print(F("mv, BG ref: "));
                 Serial.print(bgRefmV);
-                Serial.print(F("mV\n"));
+                Serial.print(F("mV\n\r"));
                 // Reset update indicator
                 nextUpdate = millis() + updateFreq;
             }
-        } else if (key==KEY_SPACE) {
+        } else if (key==KEY_SPC) {
             // Space toggles between states
             state = state==FT_STATE_MENU ? FT_STATE_TUNE : FT_STATE_MENU;
             // Show the menu if we moved to menu state
             if (state == FT_STATE_MENU)
                 Serial.print(menu);
-        } else if (key==KEY_ESC) {
+        } else if (key==KEY_ESC || key==KEY_q) {
             // Escpae key pressed
             // When in menu state, we exit fine tuning
             if(state==FT_STATE_MENU) return;
@@ -309,21 +314,21 @@ void PrecisionADC::calibrateBG() {
             Serial.print(menu);
         } else if (state==FT_STATE_MENU) {
             // In menu state we still need to deal with the EEPROM save and get
-            if (key!=KEY_ONE && key!=KEY_TWO) {
+            if (key!=KEY_1 && key!=KEY_2) {
                 // None of the keys we want, so get out of here
                 continue;
             }
-            if(key==KEY_TWO) {
+            if(key==KEY_2) {
                 // Save our current bandgap reference value to EEPROM
                 toEEPROM();
-                Serial.print(F("\nSaved to EEPROM.\n\n"));
+                Serial.print(F("\n\rSaved to EEPROM.\n\r\n\r"));
             } else {
                 // Get any saved reference from EEPROM
                 if (fromEEPROM()) {
-                    Serial.print(F("Retrieved saved value from EEPROM.\n\n"));
+                    Serial.print(F("Retrieved saved value from EEPROM.\n\r\n\r"));
                 } else {
                     // No previously saved value was found.
-                    Serial.print(F("\nNo saved bandgap value found in EEPROM.\n"));
+                    Serial.print(F("\n\rNo saved bandgap value found in EEPROM.\n\r"));
                     // We want to stay in menu state, so we continue the loop
                     continue;
                 }
@@ -332,10 +337,10 @@ void PrecisionADC::calibrateBG() {
             state = FT_STATE_TUNE;
         } else {
             // State is tuning, so deal with adjustments
-            if(key==KEY_UP) {
+            if(key==KEY_AUP || key==KEY_k) {
                 Serial.println(F("[up]"));
                 bgRefmV++;
-            } else if (key==KEY_DOWN) {
+            } else if (key==KEY_ADN || key==KEY_j) {
                 Serial.println(F("[down]"));
                 bgRefmV--;
             }
@@ -357,7 +362,7 @@ void PrecisionADC::calibrateBG() {
  *
  * @param pin (uint16_t): The analog pin to sample for the voltage reading
  * @return (uint16_t): The representative voltage in millivolt
- * 
+ *
  * @todo: We may need some buffering and summing/averaging to increase accuracy
  * since there are still some very small errors.
  **/
--- PrecisionADC.h
+++ PrecisionADC.h
@@ -13,13 +13,47 @@
 // in mV.
 #define BGREFmV 1100
 
-// Key codes and results definitions for readSerial()
-#define KEY_UP     1
-#define KEY_DOWN   2
-#define KEY_SPACE  3
-#define KEY_ESC    4
-#define KEY_ONE    5
-#define KEY_TWO    6
+// ## Support and config for readSerial() method ## //
+// Defines for detecting and stripping CR and LF characters
+#define CR 0x0D     // Carraige return
+#define LF 0x0A     // Line feed
+
+// This defines the maximum number of bytes that can be expected for multi-byte
+// keys. It is used as the second dimension for the multi dimensional keyTab
+// array defined below.
+#define KBUFSZ 3
+// Keybord scan table for keys to detect and decode. This is a multi
+// dimensional array of key, or multi-byte key definitions.
+// To make it easy to use and define the table, add macro names as comments
+// after each key/multi-key definition. Then you can use your favourite editor
+// to extract those macro names and convert them to #defines with the value of
+// each macro being the index in the table for the key/multi-key.
+const char keyTab[][KBUFSZ] = {
+                                  {'1'},        // KEY_1
+                                  {'2'},        // KEY_2
+                                  {' '},        // KEY_SPC
+                                  {27, 91, 65}, // KEY_AUP  - Up arrow
+                                  {27, 91, 66}, // KEY_ADN  - Down arrow
+                                  {27},         // KEY_ESC  - Escape
+                                  {'q'},        // KEY_q
+                                  {'j'},        // KEY_j
+                                  {'k'},        // KEY_k
+                               };
+// Calculate the number of keys in the scan table
+const uint8_t keyTabLen = sizeof(keyTab) / (KBUFSZ * sizeof(char));
+// Macro definitions for the key def indexes in keyTab
+#define NO_KEY   -1
+#define KEY_1     0
+#define KEY_2     1
+#define KEY_SPC   2
+#define KEY_AUP   3
+#define KEY_ADN   4
+#define KEY_ESC   5
+#define KEY_q     6
+#define KEY_j     7
+#define KEY_k     8
+// END ## Support and config for readSerial() method ## //
+
 
 // State names for calibrateBG() method
 #define FT_STATE_MENU 'A'
@@ -35,14 +69,14 @@ typedef struct {
 // This is the address in the EEPROM of where to write the bandgap ref. Since
 // the assumption is that most utilities that uses EEPROM will write to the
 // start, we will default to writing to very end of EEPROM.
-// The last EEPROM address is defined by the symbol E2END (see 
+// The last EEPROM address is defined by the symbol E2END (see
 //   http://www.nongnu.org/avr-libc/user-manual/group__avr__io.html )
 const uint16_t EEPROMAddy = E2END - sizeof(bgMem);
 
 class PrecisionADC {
         uint16_t bgRefmV;   // Bandgap reference voltage in mV. 1100mV default
         uint32_t bgADC();   // Reads the bandgap ADC value relative to VCC
-        uint8_t readSerial(uint8_t tout=100); // Monitors and decodes serial in
+        int8_t readSerial(uint8_t tout=100); // Monitors and decodes serial in
     public:
         // Constructor to use default BGREFmV bandgap or from EEPROM if available
         PrecisionADC() : bgRefmV(BGREFmV) { fromEEPROM(); };
